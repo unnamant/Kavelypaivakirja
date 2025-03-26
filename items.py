5b481558a3c95e76dcb7aef0b46fc6b5c03e79ce
unnamant <unna.mantyniemi@helsinki.fi>
2025-03-26 15:42:20 +0200

    Show item info

--- items.py
+++ items.py
@@ -3,3 +3,20 @@ import db
 def add_items(title, description, distance, city, user_id):
     sql = """INSERT INTO items (title, description, distance, city, user_id) VALUES (?, ?, ?, ?, ?)"""
     db.execute(sql, [title, description, distance, city, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+                    items.description,
+                    items.distance,
+                    items.city,
+                    users.username
+            FROM items, users
+            WHERE items.user_id = users.id
+            AND items.id = ? """
+
+    return db.query(sql, [item_id])[0]
