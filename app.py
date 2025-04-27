44e53303ae5dee32edb3bc140f5e9b894c6cb809
unnamant <unna.mantyniemi@helsinki.fi>
2025-04-27 17:19:49 +0300

    Add csrf check to forms

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import secrets
 import sqlite3
 from flask import Flask
 from flask import abort, flash, make_response, redirect, render_template, request, session
@@ -12,6 +13,12 @@ app.secret_key = config.secret_key
 
 def require_login():
     if "user_id" not in session:
+        abort(403)
+
+def check_csrf():
+    if "csfr_token" not in request.form:
+        abort(403)
+    if request.form["csrf_token"] != session["csrf_token"]:
         abort(403)
 
 @app.route("/")
@@ -55,7 +62,8 @@ def new_items():
 
 @app.route("/create_items", methods=["POST"])
 def create_items():
-    require_login
+    require_login()
+    check_csrf()
 
     title = request.form["title"]
     if not title or len(title) > 50:
@@ -64,7 +72,7 @@ def create_items():
     if not description or len(description) > 1000:
         abort(403)
     distance = request.form["distance"]
-    if not re.search("^[1-9][0-9]{0,3}$", distance):
+    if not re.search("^[1-9][0-9]{0,2}$", distance):
         abort(403)
     city = request.form["city"]
     user_id = session["user_id"]
@@ -90,6 +98,7 @@ def create_items():
 @app.route("/create_com", methods=["POST"])
 def create_com():
     require_login()
+    check_csrf()
 
     description = request.form["description"]
     if not description or len(description) > 1000:
@@ -145,6 +154,7 @@ def edit_images(item_id):
 @app.route("/add_image", methods=["POST"])
 def add_image():
     require_login()
+    check_csrf()
 
     item_id = request.form["item_id"]
     item = items.get_item(item_id)
@@ -170,6 +180,7 @@ def add_image():
 @app.route("/delete_images", methods=["POST"])
 def delete_images():
     require_login()
+    check_csrf()
 
     item_id = request.form["item_id"]
     item = items.get_item(item_id)
@@ -233,6 +244,8 @@ def delete_item(item_id):
         return render_template("delete_item.html", item=item)
 
     if request.method == "POST":
+        check_csrf()
+
         if "delete" in request.form:
             items.delete_item(item_id)
             return redirect("/")
@@ -284,6 +297,7 @@ def login():
         if user_id:
             session["user_id"] = user_id
             session["username"] = username
+            session["csrf_token"] = secrets.token_hex(16)
             return redirect("/")
         else:
             flash("VIRHE: väärä tunnus tai salasana")
